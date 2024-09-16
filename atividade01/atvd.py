2d9a1fa727c98c68291ac19c926014365f378a1e
Rossana Lícya <168486966+licyafreoli@users.noreply.github.com>
2024-09-16 18:49:46 -0300

    Add files via upload

---
+++ atividade01/atvd.py
@@ -0,0 +1,9 @@
+idade1 = int(input("Digite a primeira idade: "))
+idade2 = int(input("Digite a segunda idade: "))
+
+if idade1 > idade2:
+    print("A primeira idade é maior que a segunda.")
+elif idade1 < idade2:
+    print("A primeira idade é menor que a segunda.")
+else:
+    print("A primeira idade é igual à segunda.")
