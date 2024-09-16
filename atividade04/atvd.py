0c9a611b1e6811399d0b718451b1a2a33eb83beb
Rossana Lícya <168486966+licyafreoli@users.noreply.github.com>
2024-09-16 18:25:56 -0300

    Add files via upload

---
+++ atividade04/atvd.py
@@ -0,0 +1,7 @@
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+
+if nota1 >= 6 and nota2 >= 6:
+    print("O aluno está aprovado, ambas as notas são maiores ou iguais a 6.")
+else:
+    print("O aluno não está aprovado, uma ou ambas as notas são menores que 6.")
